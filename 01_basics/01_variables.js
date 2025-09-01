89932766959a7a12abe713260b9bb129af9c1014
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-01 05:55:11 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1445532
+let accountEmail = "Sajanmahla965@gmail.com"
+var acoountPassword = "SAJANmahla@26"
+accountCity = "Faridkot"
+
+
+// account = 2 Can't be changed.
+// as we cann see its a. CONSTANT.(if you are a swift programmer then think it like a LET. mean it cant be changed. )
+
+accountEmail = "rea@google.com"
+accountPassword = "bxhiyf732"
+accountCity = "Cupertino"
+
+
+
+console.table([accountId, accountEmail, acoountPassword, accountCity])
+/*
+prefer not to use VARiable
+beacuse issue of the issue in the block scope and functional scope
+*/
