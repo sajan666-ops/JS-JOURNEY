b7a27384f0b81d0bb70f33625e2d38dfb90972e0
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-01 06:30:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+// so today its time for datatypes and lemme tell you what i learnt today.
+
+let myName = "Sajan" // --> this is String type
+let age = 18 // --> this is [INT]eger type
+let islogged = true // --> this is Bollean type means it holds only true or false
+let obj = null // --> this is Null type mean its standalone object
+let und; // its undefined like we ourself havent put any sort of thing so if we put "" then i can be string.
+
+console.table([myName, age, islogged, obj, und])
+
+
+//  SO AFTER RUNNING THE COMMAND IN. TERMINAL WE GOT THIS.
+// the command is ----> node 01_basics/02_dataTypes
+/*
+┌─────────┬───────────┐
+│ (index) │ Values    │
+├─────────┼───────────┤
+│ 0       │ 'Sajan'   │
+│ 1       │ 18        │
+│ 2       │ true      │
+│ 3       │ null      │
+│ 4       │ undefined │
+└─────────┴───────────┘
+
+*/
