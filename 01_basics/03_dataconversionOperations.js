8f76a81aba686c90799719b09635497daf80ec15
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-01 10:32:07 +0000

    data conversions

---
+++ 01_basics/03_dataconversionOperations.js
@@ -0,0 +1,35 @@
+// so lemme show you how to do dataypes conversion in javascript. 
+// so we'll assign the value to with any data 
+
+
+
+/*  CONVERTING THE NUMBER/INTERGER TO STRING */
+let age = 18   // --> so here we took a number/integer as its value 18
+console.log(age) // here we printing its key(AGE).
+console.log(typeof age) // here we are also printing the type of the age which is number
+let updatedAge = String(age) // here we are updatig the value from number to string 
+console.log(updatedAge) // here the upadated value is also 18. but this time its string istead of integer 
+console.log(typeof updatedAge) // here we are printing the type of updated type of the age which is now string
+
+
+/* CONVERTING THE NUMBER/STRING TO BOLLEAN */
+let boolType = 0 // boolType = "Apple" /* here we are taking the boolType = 0 = False or boolType = "Apple" = True */
+console.log(boolType) // here we. are printing the value of booltype which is 0 for now
+console.log(typeof boolType) // here we are printing its type 
+let UpdatedType = Boolean(boolType) // after that so in this line we are changing the previous type to bool type. 
+console.log(UpdatedType) //. here we are printing the updated value 
+console.log(typeof UpdatedType) // at last we. are checking the type of updatedType.
+
+
+/* SO THIS IS MY COMMAND RESULT: num to string
+18 --> assign 
+number --> type
+18 --> changed from number to string
+string --> now its string 
+_____________________________________________________
+num/string to boolean
+0 --> assign a value
+number --> type 
+false --> changed from number to boolean
+boolean --> now its boolean
+*/ 
