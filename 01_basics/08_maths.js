e16f385e01b066d970106739ca71daaa22c031c5
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-04 08:34:59 +0000

    maths

---
+++ 01_basics/08_maths.js
@@ -0,0 +1,28 @@
+// so here i learned about MATHS in JAVASCRIPT.
+
+console.log(Math) // so this is used to output the simple maths function
+console.log(Math.abs(-33)) // this is absolute. it is used to convert the negative number to positive 
+console.log(Math.round(4.3)) // this is use the round off the decimal number
+console.log(Math.ceil(6.1)) // it is always used to round to round off to bigger side.
+console.log(Math.floor(9.9)) // it is always used to round to round off to smaller side.
+console.log(Math.min(4,7,2,8)) // it is used to find the minimum of all the numbers
+console.log(Math.max(4,7,2,8)) // it is used to find the maximum of all the numbers
+console.log(Math.random()) // it is used to get the random number. these numbers are very low 
+console.log(Math.random()*10 + 1) // by this we can get the number which will be start from one
+
+let max = 20 // here we assign the max number 
+let min = 10 // here we assign the min number 
+
+console.log(Math.floor(Math.random() * max - min + 1) + min) // by this we can get the random number in bw MIN and MAX.
+
+/* OUTPUT:
+33
+4
+7
+9
+2
+8
+0.19903784313159667
+5.433585628815853
+2
+*/
