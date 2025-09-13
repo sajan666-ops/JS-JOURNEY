d6f9b2c0fafc16dc2dc0eb77bf25e7716eead25c
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-13 15:57:26 +0530

    Create 04_ForIn.js

---
+++ 05_iteration/04_ForIn.js
@@ -0,0 +1,19 @@
+let myObject = {
+    name : "Sajan",
+    age : 19,
+    gender: "Male",
+    city: "Faridkot"
+}
+for (const key in myObject) {
+  console.log(myObject[key]);
+    
+}
+
+const programming = ["Py","Cpp","Js","Ts","Html","Go"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+    
+    
+}
